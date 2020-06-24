beb05c73eb92131f1f5339bf237ce5bd6aa16dd9
Miguel Angel Parada Cañon <michael_v613@hotmail.com>
2020-06-23 22:07:46 -0500

    Update
    Save without CORS request, seeing if request is successfull

--- js/sound.js
+++ js/sound.js
@@ -34,7 +34,8 @@ SoundCloudAPI = {};
 
 SoundCloudAPI.init = function () {
     SC.initialize({
-        client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+        client_id: 'cd9be64eeb32d1741c17cb39e41d254d',
+        redirect_uri: 'http://api.soundcloud.com'
     });
 }
 
